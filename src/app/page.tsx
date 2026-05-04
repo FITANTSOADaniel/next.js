339b554bb178252421d96d7a178504150a64d978
Fitantsoa Daniel <fitantsoarabemanantsoa@gmail.com>
2026-05-04 14:59:14 -0800

    add components

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,10 +1,39 @@
-import {Button} from "@/styles/components/ui/button";
+"use client"
+
+import {
+  AlertDialog,
+  AlertDialogTrigger,
+  AlertDialogContent,
+  AlertDialogHeader,
+  AlertDialogTitle,
+  AlertDialogDescription,
+  AlertDialogFooter,
+  AlertDialogCancel,
+  AlertDialogAction,
+} from "@/components/ui/alert-dialog"
+
+import { Button } from "@/components/ui/button"
 
 export default function Home() {
   return (
-    <div>
-      <h1>Hello next js</h1>
-      <Button>Click me</Button>
-    </div>
-  );
+    <AlertDialog>
+      <AlertDialogTrigger>
+        <Button variant="outline">Show Dialog</Button>
+      </AlertDialogTrigger>
+
+      <AlertDialogContent>
+        <AlertDialogHeader>
+          <AlertDialogTitle>Are you absolutely sure?</AlertDialogTitle>
+          <AlertDialogDescription>
+            This action cannot be undone.
+          </AlertDialogDescription>
+        </AlertDialogHeader>
+
+        <AlertDialogFooter>
+          <AlertDialogCancel>Cancel</AlertDialogCancel>
+          <AlertDialogAction>Continue</AlertDialogAction>
+        </AlertDialogFooter>
+      </AlertDialogContent>
+    </AlertDialog>
+  )
 }
